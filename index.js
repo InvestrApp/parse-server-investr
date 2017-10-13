debe3c52bc94cb1d1edf9397caed0fd4d6b627b5
Sahil Agarwal <sahildagarwal@gmail.com>
2017-10-12 20:43:15 -0700

    Updated server settings

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'investrAndroid',
+  masterKey: process.env.MASTER_KEY || 'investrParse2017', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://investr-android.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -36,7 +36,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.status(200).send('Get excited for Investr!');
 });
 
 // There will be a test page available on the /test path of your server url
@@ -48,7 +48,7 @@ app.get('/test', function(req, res) {
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log('parse-server-investr-android running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
